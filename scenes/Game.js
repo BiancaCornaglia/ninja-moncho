a95907bb9b889a321b46108bd1734a6004ab5f52
Bianca Cornaglia <bian.cornaglia11@gmail.com>
2024-06-04 15:07:16 -0300

    agrego assets y cambio nombre de variables

--- scenes/Game.js
+++ scenes/Game.js
@@ -21,40 +21,42 @@ export default class Game extends Phaser.Scene {
     //cargar assets
 
     //import Cielo
-    this.load.image("cielo", "../public/assets/Cielo.webp");
+    this.load.image("sky", "../public/assets/Cielo.webp");
 
     //import plataforma
-    this.load.image("plataforma", "../public/assets/platform.png");
+    this.load.image("platform", "../public/assets/platform.png");
 
     //import personaje
-    this.load.image("personaje", "../public/assets/Ninja.png");
+    this.load.image("character", "../public/assets/pj04.png");
 
     // importar recolectable
-    this.load.image("triangulo", "../public/assets/triangle.png");
-    this.load.image("cuadrado", "../public/assets/square.png");
-    this.load.image("rombo", "../public/assets/diamond.png");
-    this.load.image("bomb", "../public/assets/bomb.png");
+    this.load.image("triangulo", "../public/assets/piñata01.png");
+    this.load.image("cuadrado", "../public/assets/piñata02.png");
+    this.load.image("rombo", "../public/assets/piñata03.png");
+    this.load.image("bomb", "../public/assets/nene01.png");
   }
 
   create() {
     // crear elementos
-    this.cielo = this.add.image(400, 300, "cielo");
-    this.cielo.setScale(2);
+    this.sky = this.add.image(400, 300, "sky");
+    this.sky.setScale(2);
 
     // crear grupa plataformas
-    this.plataformas = this.physics.add.staticGroup();
+    this.platform = this.physics.add.staticGroup();
     // al grupo de plataformas agregar una plataforma
-    this.plataformas.create(400, 568, "plataforma").setScale(2).refreshBody();
+    this.platform.create(400, 568, "platform").setScale(2).refreshBody();
     // agregamos otra plataforma en otro lugar
-    this.plataformas.create(200, 400, "plataforma");
+    this.platform.create(20, 400, "platform");
+
+    this.platform.create(800, 400, "platform");
 
     //crear personaje
-    this.personaje = this.physics.add.sprite(400, 300, "personaje");
-    this.personaje.setScale(0.1);
-    this.personaje.setCollideWorldBounds(true);
+    this.character = this.physics.add.sprite(400, 300, "character");
+    //this.personaje.setScale(0.1);
+    this.character.setCollideWorldBounds(true);
 
     //agregar colision entre personaje y plataforma
-    this.physics.add.collider(this.personaje, this.plataformas);
+    this.physics.add.collider(this.character, this.platform);
     //una tecla a la vez
     //this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     //this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -65,7 +67,7 @@ export default class Game extends Phaser.Scene {
     this.cursor = this.input.keyboard.createCursorKeys();
 
     // crear grupo recolectables
-    this.recolectables = this.physics.add.group();
+    this.collectable = this.physics.add.group();
 
     // evento 1 segundo
     this.time.addEvent({
@@ -103,8 +105,8 @@ export default class Game extends Phaser.Scene {
 
     //agregar collider entre recolectables y personaje
     this.physics.add.collider(
-      this.personaje,
-      this.recolectables,
+      this.character,
+      this.collectable,
       this.onShapeCollect,
       null,
       this
@@ -112,8 +114,8 @@ export default class Game extends Phaser.Scene {
 
     //agregar collider entre recolectables y plataformas
     this.physics.add.collider(
-      this.recolectables,
-      this.plataformas,
+      this.collectable,
+      this.platform,
       this.onRecolectableBounced,
       null,
       this
@@ -131,14 +133,14 @@ export default class Game extends Phaser.Scene {
     }
     // movimiento personaje
     if (this.cursor.left.isDown) {
-      this.personaje.setVelocityX(-160);
+      this.character.setVelocityX(-160);
     } else if (this.cursor.right.isDown) {
-      this.personaje.setVelocityX(160);
+      this.character.setVelocityX(160);
     } else {
-      this.personaje.setVelocityX(0);
+      this.character.setVelocityX(0);
     }
-    if (this.cursor.up.isDown && this.personaje.body.touching.down) {
-      this.personaje.setVelocityY(-330);
+    if (this.cursor.up.isDown && this.character.body.touching.down) {
+      this.character.setVelocityY(-330);
     }
   }
 
@@ -150,34 +152,34 @@ export default class Game extends Phaser.Scene {
     const tipos = ["triangulo", "cuadrado", "rombo", "bomb"];
 
     const tipo = Phaser.Math.RND.pick(tipos);
-    let recolectable = this.recolectables.create(
+    let collectable = this.collectable.create(
       Phaser.Math.Between(10, 790),
       0,
       tipo
     );
-    recolectable.setVelocity(0, 100);
+    collectable.setVelocity(0, 100);
 
     //asignar rebote: busca un numero entre 0.4 y 0.8
     const rebote = Phaser.Math.FloatBetween(0.4, 0.8);
-    recolectable.setBounce(rebote);
+    collectable.setBounce(rebote);
 
     //set data
-    recolectable.setData("points", this.shapes[tipo].points);
-    recolectable.setData("tipo", tipo);
+    collectable.setData("points", this.shapes[tipo].points);
+    collectable.setData("tipo", tipo);
   }
 
-  onShapeCollect(personaje, recolectable) {
-    const nombreFig = recolectable.getData("tipo");
-    const points = recolectable.getData("points");
+  onShapeCollect(character, collectable) {
+    const nombreFig = collectable.getData("tipo");
+    const points = collectable.getData("points");
 
     this.score += points;
 
     this.shapes[nombreFig].count += 1;
 
     console.table(this.shapes);
-    console.log("recolectado ", recolectable.texture.key, points);
+    console.log("recolectado ", collectable.texture.key, points);
     console.log("score ", this.score);
-    recolectable.destroy();
+    collectable.destroy();
     //recolectable.disableBody(true, true);
 
     this.scoreText.setText(
@@ -218,13 +220,13 @@ export default class Game extends Phaser.Scene {
     }
   }
 
-  onRecolectableBounced(recolectable, plataforma) {
+  onRecolectableBounced(collectable, platform) {
     console.log("recolectable rebote");
-    let points = recolectable.getData("points");
+    let points = collectable.getData("points");
     points -= 5;
-    recolectable.setData("points", points);
+    collectable.setData("points", points);
     if (points <= 0) {
-      recolectable.destroy();
+      collectable.destroy();
     }
   }
 }
